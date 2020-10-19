a298ca27fa9adfe88d36d57daec16e2c7c8babad
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-10-19 22:45:30 +0200

    Added Bellman Ford algorithm (Implementation 1)

---
+++ Graph/BellmanFord.cpp
@@ -0,0 +1,45 @@
+struct Edge{
+
+    int from, to, cost;
+
+};
+
+/// Bellman Ford Algorithm:
+// Time Complexity: O(n * m)
+// 1- to find single source shortest path for weighted directed graphs with negative edges.
+// 2- to detect negative cycles
+
+bool BellmanFord(int src, vector<Edge> & edges, vector<int> & d, int nodes) {
+
+    // returns true if there's no negative cycle and the vector d sent by reference has the answer
+    // returns false if there's a negative cycle
+    // don't forget to assign OO bigger than any possible path
+
+    d.assign(nodes, OO);
+    d[src] = 0;
+
+    for (int i = 1; i <= nodes; ++i) {
+
+        bool relaxed = false;
+
+        for(auto & e : edges) {
+
+            if(d[e.from] < OO) {
+
+                if(d[e.to] > d[e.from] + e.cost) {
+
+                    d[e.to] = max(-OO, d[e.from] + e.cost);
+                    relaxed = true;
+
+                }
+
+            }
+
+        }
+
+        if(!relaxed) return true;
+        if(i == nodes) return false;
+
+    }
+
+}
