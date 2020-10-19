8805118970a82261e75bfc7e5836aa3d0df2bbc3
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-10-19 22:27:53 +0200

    Added comments to explain functions.

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -3,3 +3,6 @@
 using namespace __gnu_pbds;
 
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+// order_of_key(k): returns the number of elements in the set strictly less than k
+// find_by_order(k): returns an iterator to the k-th element (zero-based) in the set
