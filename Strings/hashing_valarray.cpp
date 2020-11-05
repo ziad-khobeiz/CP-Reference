50c73df922a8d1cc5de7e5a99f6477512df10514
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-05 20:45:02 +0200

    Update hashing_valarray.cpp

--- Strings/hashing_valarray.cpp
+++ Strings/hashing_valarray.cpp
@@ -1,22 +1,7 @@
-#include <bits/stdc++.h>
-
-#define ll long long
-#define ld long double
-
-using namespace std;
-
-const int N = 1e5 + 5;
-const int M = 2000 + 5;
-const int OO = 1e9;
-const int mod = 1e9 + 7;
-
 typedef valarray<ll> hashing;
-
-int t, n, k;
 hashing MOD = {(ll)1e9 + 7, (ll)1e9 + 9};
 hashing B = {(ll)31, (ll)53};
 hashing h[N], pw[N];
-string s;
 set<pair<ll, ll> > hashes;
 
 hashing add(const hashing & a, const hashing & b) {
@@ -39,43 +24,29 @@ void init() {
 
 int main() {
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-    #else
-        #endif
-
     pw[0] = {1, 1};
-
     for (int i = 1; i < N; ++i) {
         pw[i] = mul(pw[i - 1], B);
     }
-
-    cin >> t;
-
-    while (t--) {
-
+    
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        
+        int n, k;
+        string s;
         cin >> n >> k >> s;
-
         init();
-
+        
         h[0] = {0, 0};
-
         for (int i = 1; i <= n; ++i) {
             h[i] = add(h[i - 1], mul(hashing(s[i - 1] - 'a' + 1, 2), pw[i - 1]));
         }
-
         for (int i = 0; i <= n - k; ++i) {
-
             auto cur = mul(add(h[i + k], MOD - h[i]), pw[n - k - i]);
             hashes.emplace(cur[0], cur[1]);
-
         }
-
         cout << hashes.size() << '\n';
-
     }
 
     return 0;
