c0494c09f5b1c7197a9a409b65505c01afdfc90d
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-05 18:36:18 +0200

    Update Floyd.cpp

--- Graph Theory/Floyd.cpp
+++ Graph Theory/Floyd.cpp
@@ -1,11 +1,22 @@
 // Take care of multiple edges (assign the cost of the edge with the minimum edge among the multiple edges)
 
+void initFloyd() {
+     for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            dis[i][j] = OO;
+        }
+        dis[i][i] = 0;
+    }
+}
+
 void Floyd(){
-    for(int k = 1; k <= n; k++){
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
-            }
+    for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            // if condition for negative edges
+            if (d[i][k] < OO && d[k][j] < OO)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
         }
     }
+}
 }
