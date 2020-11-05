bfae0024525237c9f84813688d9537014ec99691
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-05 21:27:15 +0200

    Update max_matching.cpp

--- Graph Theory/Flows/max_matching.cpp
+++ Graph Theory/Flows/max_matching.cpp
@@ -23,14 +23,16 @@ bool can(int node){
     return 0;
 }
 
+void init() {
+    memset(left_m, -1, sizeof left_m);
+}
 
 int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int c = 0;
     for(int i = 0; i < n; i++){
+        // Memset vis between calls of can() function
         for(int j = 0; j < n; j++) vis[j] = 0;
         if(can(i)) c++;
     }
-    
     return 0;
 }
