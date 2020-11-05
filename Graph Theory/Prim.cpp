916fdf0ad5ab0d151b399c0a0a4869b1ac2167c1
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-05 21:00:42 +0200

    Create Prim.cpp

---
+++ Graph Theory/Prim.cpp
@@ -0,0 +1,40 @@
+struct Edge{
+
+    int from, to;
+    double cost;
+
+    // The operator is reversed to sort the priority_queue non-increasing
+    bool operator < (const Edge &e) const {
+        return cost > e.cost;
+    }
+
+};
+
+bool vis[N];
+
+ll Prim() {
+
+    priority_queue<Edge> q;
+
+    memset(vis, 0, sizeof vis);
+
+    q.push({-1, 0, 0});
+
+    ll ret = 0;
+
+    while (!q.empty()) {
+        auto e = q.top();
+        q.pop();
+        if(vis[e.to]) continue;
+        vis[e.to] = true;
+        ret += e.cost;
+        for (int i = 0; i < n; ++i) {
+            if(!vis[i]) {
+                q.push({e.to, i, dist(e.to, i)});
+            }
+        }
+    }
+
+    return ret;
+
+}
