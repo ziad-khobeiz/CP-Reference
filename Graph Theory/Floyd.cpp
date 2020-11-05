f86e6bded4d62f0e733d6b604122e0a3c0b89763
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-05 18:37:23 +0200

    Update Floyd.cpp

--- Graph Theory/Floyd.cpp
+++ Graph Theory/Floyd.cpp
@@ -10,9 +10,9 @@ void initFloyd() {
 }
 
 void Floyd(){
-    for (int k = 0; k < n; ++k) {
-         for (int i = 0; i < n; ++i) {
-             for (int j = 0; j < n; ++j) {
+    for (int k = 1; k <= n; ++k) {
+         for (int i = 1; i <= n; ++i) {
+             for (int j = 1; j <= n; ++j) {
                  // if condition for negative edges
                  if (d[i][k] < OO && d[k][j] < OO)
                      d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
