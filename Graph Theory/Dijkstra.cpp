208c9ba70c48cdb2c02cfd1cfc9a1364a95697d9
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-05 18:51:55 +0200

    Update Dijkstra.cpp

--- Graph Theory/Dijkstra.cpp
+++ Graph Theory/Dijkstra.cpp
@@ -1,6 +1,5 @@
 // Dijkstra O(m * log)
 
-const int N = 1e5 + 5;
 struct edge {
     int to;
     long long cost;
@@ -8,10 +7,11 @@ struct edge {
 
 long long dist[N];
 vector<edge> adj[N];
+
 void Dijkstra(int s) {
-    //                 dist,node
+    //                 <dist,node
     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
-    fill(dist, dist + N, 1e18);
+    fill(dist, dist + N, OO);
     pq.push({s, 0});
     dist[s] = 0;
     while (pq.size()) {
@@ -22,7 +22,7 @@ void Dijkstra(int s) {
         for (auto &c : adj[node]) {
             if (dist[c.to] > dist[node] + c.cost) {
                 dist[c.to] = dist[node] + c.cost;
-                pq.push({c.to, dist[c.to]});
+                pq.push({dist[c.to], c.to});
             }
         }
     }
