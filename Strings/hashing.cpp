3f0df571aa4544d32e11834a84fd278e00544eae
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-10 22:21:49 +0200

    Update hashing.cpp

--- Strings/hashing.cpp
+++ Strings/hashing.cpp
@@ -48,3 +48,30 @@ void build_hash(string &s){
         pref2[i] = add(pref2[i - 1], mul(pw2[i], s[i] - 'a' + 1));
     }
 }
+
+int count_unique_substrings(string const& s) {
+    int n = s.size();
+
+    const int p = 31;
+    const int m = 1e9 + 9;
+    vector<long long> p_pow(n);
+    p_pow[0] = 1;
+    for (int i = 1; i < n; i++)
+        p_pow[i] = (p_pow[i-1] * p) % m;
+
+    vector<long long> h(n + 1, 0);
+    for (int i = 0; i < n; i++)
+        h[i+1] = (h[i] + (s[i] - 'a' + 1) * p_pow[i]) % m;
+
+    int cnt = 0;
+    for (int l = 1; l <= n; l++) {
+        set<long long> hs;
+        for (int i = 0; i <= n - l; i++) {
+            long long cur_h = (h[i + l] + m - h[i]) % m;
+            cur_h = (cur_h * p_pow[n-i-1]) % m;
+            hs.insert(cur_h);
+        }
+        cnt += hs.size();
+    }
+    return cnt;
+}
