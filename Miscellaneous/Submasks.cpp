b847d275ed8804780efb2eedb41154a2f8941590
Ziad Khobeiz <54692663+ziad-khobeiz@users.noreply.github.com>
2020-11-10 21:46:32 +0200

    Create Submasks.cpp

---
+++ Miscellaneous/Submasks.cpp
@@ -0,0 +1,8 @@
+// iterate over all the masks
+for (int mask = 0; mask < (1<<n); mask++){
+	F[mask] = A[0];
+    // iterate over all the subsets of the mask
+    for(int i = mask; i > 0; i = (i-1) & mask){
+    	F[mask] += A[i];
+    }
+}
